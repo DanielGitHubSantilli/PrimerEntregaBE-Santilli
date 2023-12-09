52cdcd87c6905fc3867aefcbddea08a897c1c837
danielsantilli <danielsantilli1@hotmail.com>
2023-12-08 22:35:07 -0300

    Agregado de Productos

--- src/manager.js
+++ src/manager.js
@@ -1,29 +1,33 @@
 import { ProductManager } from "./ProductManager.js";
 
-let miPrimeraTienda = new ProductManager("./products.json");
+let CAIDIM = new ProductManager("./productos.json");
 
 //title, description, price, thumbnail, code, stock
-miPrimeraTienda.addProduct(
-  "Arroz",
-  "Arroz de bolsa",
+const Detergente = "https://i.postimg.cc/YjcjJzVC/133791-OSAS83-455.jpg";
+const Cif = "https://i.postimg.cc/gcxsbc5C/cif.png";
+const Ayudin = "https://i.postimg.cc/0QTwsvKv/Ayudin1-Litro.jpg"
+
+CAIDIM.addProduct(
+  "Detergente",
+  "Detergente liquido",
   200,
-  "sin imagen",
+  Detergente,
+  "001",
+  10
+);
+CAIDIM.addProduct(
+  "Cif",
+  "Cif 100 cm3",
+  200,
+  Cif,
+  "004",
+  10
+);
+CAIDIM.addProduct(
+  "Ayudin",
+  "Ayudin 1000 cm3",
+  200,
+  Ayudin,
   "005",
-  10
-);
-miPrimeraTienda.addProduct(
-  "Leche",
-  "Leche descremada",
-  200,
-  "sin imagen",
-  "002",
-  10
-);
-miPrimeraTienda.addProduct(
-  "Huevos",
-  "Huevos de gallina",
-  200,
-  "sin imagen",
-  "003",
   10
 );
